6c3e6796432fc3dc5ec1f7532d64d8e6840ed0cb
Aritra Sengupta <aritra55@gmail.com>
2021-11-27 00:39:40 +0530

    cleaning up the code and code restructure

--- src/app.js
+++ src/app.js
@@ -24,50 +24,7 @@ import { CameraControl } from "./CameraControl";
 
 window.onload = async function() {
   if (WEBGL.isWebGLAvailable()) {
-    // Initiate function or other initializations here
-    /* const canvas = document.querySelector("#c");
-    if (!canvas) {
-      throw new Error("Canvas is not setup properly");
-    }
-    const space = new ThreeDSpace(canvas);
-
-    space.init();
-    await space.loadModel();
-
-    const {
-      scene,
-      camera,
-      renderer,
-      mixer,
-      neck,
-      waist,
-      idle,
-      possibleAnims
-    } = space;
-
-    const animation = new ModelAnimation(scene, camera, renderer, mixer);
-    animation.update();
-
-    const { raycast, getMousePos, moveJoint } = animation;
-    window.addEventListener("click", e =>
-      raycast(e, false, idle, possibleAnims)
-    );
-    window.addEventListener("touchend", e =>
-      raycast(e, true, idle, possibleAnims)
-    );
-
-    document.addEventListener("mousemove", function(e) {
-      const mousecoords = getMousePos(e);
-      if (neck && waist) {
-        moveJoint(mousecoords, neck, 50);
-        moveJoint(mousecoords, waist, 30);
-      }
-    }); */
-    let APP = null;
-    // APP = new BasicWorldDemo();
-    // APP = new LoadModelDemo();
-    // APP = new CharacterController();
-    APP = new CameraControl();
+    new CameraControl();
   } else {
     const warning = WEBGL.getWebGLErrorMessage();
     document.getElementById("container").appendChild(warning);
